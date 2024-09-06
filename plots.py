2c23485a8442b7169c5f5c0001feb6f6e870f20a
Vivian Kessler <vivian.kessler@hispeed.ch>
2024-09-06 15:24:11 +0200

    Heston process

--- plots.py
+++ plots.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def histogram(array, label, color):
-    plt.hist(array, color=color, label=label, density=True, bins="auto", histtype="stepfilled", alpha=.3)
+    plt.hist(array, color=color, label=label, density=True, bins=500, histtype="stepfilled", alpha=.7)
     mean, std = np.mean(array), np.std(array)
     ymin, ymax = plt.ylim()
     plt.vlines([mean - std, mean, mean + std], colors=color, ymin=ymin, ymax=ymax)
