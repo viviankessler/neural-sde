acfdc712bf78580c046e6bcc9d867699ba864345
Vivian Kessler <vivian.kessler@hispeed.ch>
2024-09-09 14:46:12 +0200

    dynamic drift limit

--- plots.py
+++ plots.py
@@ -4,6 +4,6 @@ import torch
 
 def histogram(array, label, color):
     array = array[~torch.isnan(array)]
-    plt.hist(array, color=color, label=label, density=True, bins=500, histtype="stepfilled", alpha=.7)
+    plt.hist(array, color=color, label=label, density=True, bins="auto", histtype="stepfilled", alpha=.7)
     mean, std = torch.mean(array), torch.std(array)
     plt.vlines([mean - std, mean, mean + std], colors=color, ymin=0, ymax=.1, linestyles="dotted")
