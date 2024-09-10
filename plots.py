55aaf3eef33a291809ce1e662bf9056ef4a6a8b5
Vivian Kessler <vivian.kessler@hispeed.ch>
2024-09-10 14:15:48 +0200

    tidy up!

--- plots.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import torch
-
-
-def histogram(array, label, color):
-    array = array[~torch.isnan(array)]
-    plt.hist(array, color=color, label=label, density=True, bins="auto", histtype="stepfilled", alpha=.7)
-    mean, std = torch.mean(array), torch.std(array)
-    plt.vlines([mean - std, mean, mean + std], colors=color, ymin=0, ymax=.1, linestyles="dotted")
