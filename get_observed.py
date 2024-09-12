564e52b46787e30a64ed358c2134e5d830537e0f
Vivian Kessler <vivian.kessler@hispeed.ch>
2024-09-12 12:38:29 +0200

    checks

--- get_observed.py
+++ get_observed.py
@@ -3,14 +3,12 @@ import pandas as pd
 import numpy as np
 import random
 
-INTERVAL = "1d"
-LOCAL_VOL_WINDOW_SIZE = 5
 
 
-def get_samples(stock_name, start, end, prediction_period, size=10):
-    observed_process = get_observed_process(stock_name, start, end, interval=INTERVAL, local_vol_window_size=LOCAL_VOL_WINDOW_SIZE)
+def get_samples(stock_name, start, end, prediction_period, interval, local_window_size, sample_size):
+    observed_process = get_observed_process(stock_name, start, end, interval=interval, local_window_size=local_window_size)
     windows = [*get_windows(observed_process, prediction_period)]
-    return random.sample(windows, size)
+    return random.sample(windows, sample_size)
 
 
 def get_windows(df, prediction_period):
@@ -20,14 +18,14 @@ def get_windows(df, prediction_period):
             yield window
 
 
-def get_observed_process(stock_name, start, end, interval, local_vol_window_size):
+def get_observed_process(stock_name, start, end, interval, local_window_size):
     
     observed_prices = get_S(stock_name, start, end, interval)
     observation_times = get_t(observed_prices)
     time_deltas = get_deltas(observation_times)
     price_deltas = get_deltas(observed_prices)
-    risk_free_rates = get_rates(start, end, interval)
-    local_volatilities = get_nu(risk_free_rates, price_deltas, observed_prices, time_deltas, local_vol_window_size)
+    risk_free_rates = get_rates(price_deltas, observed_prices, time_deltas, local_window_size)
+    local_volatilities = get_nu(risk_free_rates, price_deltas, observed_prices, time_deltas, local_window_size)
     local_volatility_deltas = get_deltas(local_volatilities)
 
     observed_process = pd.DataFrame({
@@ -35,6 +33,7 @@ def get_observed_process(stock_name, start, end, interval, local_vol_window_size
         "r": risk_free_rates,
         "S": observed_prices,
         "ν": local_volatilities,
+        "theta": local_volatilities.rolling(10 * local_window_size, min_periods=local_window_size).mean(),
         "_time_deltas": time_deltas,
         "_price_deltas": price_deltas,
         "_loc_vol_deltas": local_volatility_deltas,
@@ -58,14 +57,11 @@ def get_deltas(array):
     return array.diff().shift(periods=-1)
 
 
-def get_rates(start, end, interval):
-    treasury_data = yf.download("CSBGC3.SW", start=start, end=end, interval=interval)["Close"]
-    observation_times = get_t(treasury_data)
-    time_deltas = get_deltas(observation_times)
-    return get_deltas(treasury_data) / treasury_data / time_deltas
+def get_rates(price_deltas, observed_prices, time_deltas, local_window_size):
+    return (price_deltas / observed_prices / time_deltas).rolling(local_window_size).mean()
 
 
-def get_nu(risk_free_rates, price_deltas, observed_prices, time_deltas, local_vol_window_size):
+def get_nu(risk_free_rates, price_deltas, observed_prices, time_deltas, local_window_size):
     numerator = price_deltas - risk_free_rates * observed_prices * time_deltas
     denominator = observed_prices * np.sqrt(time_deltas)
-    return (numerator / denominator).rolling(local_vol_window_size).var()
+    return (numerator / denominator).rolling(local_window_size).var()
--- heston.py
+++ heston.py
@@ -15,11 +15,11 @@ class Heston(torch.nn.Module):
         if eta < 0:
             raise AssertionError("eta must be positive")
         
-        self.r = torch.nn.Parameter(torch.tensor(r), requires_grad=False)
-        self.eta = torch.nn.Parameter(torch.tensor(eta), requires_grad=False)
-        self.theta = torch.nn.Parameter(torch.tensor(theta), requires_grad=False)
-        self.xi = torch.nn.Parameter(torch.tensor(xi), requires_grad=False)
-        self.rho = torch.nn.Parameter(torch.tensor(rho), requires_grad=False)  # TODO
+        self.r = torch.nn.Parameter(torch.tensor(r))
+        self.eta = torch.nn.Parameter(torch.tensor(eta))
+        self.theta = torch.nn.Parameter(torch.tensor(theta))
+        self.xi = torch.nn.Parameter(torch.tensor(xi))
+        self.rho = torch.nn.Parameter(torch.tensor(rho))  # TODO
 
     def f(self, t, y):
         S, nu = self.get_S_nu(y)
@@ -34,33 +34,29 @@ class Heston(torch.nn.Module):
         diffusion_nu = torch.sqrt(nu) * self.xi
         return torch.stack([diffusion_S, diffusion_nu], dim=-1)
     
-    @staticmethod
-    def get_S_nu(y):
+    def get_S_nu(self, y):
         y = torch.maximum(1e-9 * torch.ones_like(y), y)
         S, nu = y[..., 0], y[..., 1]
         return S, nu
     
     def predict(self, trajectory, num_simulations, dt):
-        t = pandas_to_tensor(trajectory["t"])
-        Y0 = pandas_to_tensor(trajectory.iloc[0][["S", "ν"]]).repeat(num_simulations, 1)
-        Y = torchsde.sdeint(self, Y0, t, dt=dt, method="euler")
-        return Y
+        with torch.no_grad():
+            t = torch.tensor(trajectory["t"])
+            Y0 = torch.tensor(trajectory.iloc[0][["S", "ν"]]).repeat(num_simulations, 1)
+            Y = torchsde.sdeint(self, Y0, t, dt=dt, method="euler")
+            return Y
 
 
 def infer_heston_parameters(observed_process):
-    r = get_r(observed_process["_price_deltas"], observed_process["S"], observed_process["_time_deltas"])
-    theta = get_theta(observed_process["ν"])
+    r = get_r(observed_process["r"])
+    theta = observed_process["theta"].mean()
     eta = get_eta(theta, observed_process["_loc_vol_deltas"], observed_process["ν"], observed_process["_time_deltas"])
     xi = get_xi(theta, eta, observed_process["_loc_vol_deltas"], observed_process["_time_deltas"], observed_process["ν"])
     return dict(r=r, theta=theta, eta=eta, xi=xi)
 
 
-def get_r(price_deltas, observed_prices, time_deltas):
-    return (price_deltas / observed_prices / time_deltas).mean()
-
-
-def get_theta(local_volatilities):
-    return local_volatilities.mean()
+def get_r(rates):
+    return rates.mean()
 
 
 def get_eta(theta, loc_vol_deltas, local_volatilities, time_deltas):
@@ -73,7 +69,3 @@ def get_xi(theta, eta, loc_vol_deltas, time_deltas, local_volatilities):
     numerator = loc_vol_deltas - eta * (theta - local_volatilities) * time_deltas
     denominator = np.sqrt(local_volatilities * time_deltas)
     return (numerator / denominator).std()
-
-
-def pandas_to_tensor(pandas_object):
-    return torch.tensor(pandas_object, requires_grad=False)
